75cb16d1fd2541a2c7dfe812a0bfad89562e9436
pruthviraj-raj <pruthvirajdubal66@gmail.com>
2026-06-16 12:18:58 +0530

    Add files via upload

---
+++ array17.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std ;
+//array short
+void shortZeroAndOnes(vector<int>&v){
+    int zeros_count =0;
+    for(int ele:v){
+        if(ele==0) {
+            zeros_count++;
+        }
+    }
+    for(int i=0;i<v.size();i++){
+        if(i<zeros_count){
+            v[i]=0;
+        }
+        else{
+            v[i]=1;
+        }
+    }
+}
+int main ()
+{
+int n;
+cin>>n;
+
+vector<int> v;
+for(int i=0;i<n;i++){
+    int ele; cin>>ele;
+    v.push_back(ele);
+}
+shortZeroAndOnes(v);
+
+for(int i=0;i<n;i++){
+    cout<<v[i]<<" ";
+}cout<<endl;
+
+    return 0;
+}
---
+++ array18.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std ;
+
+void shortedSquaredArray(vector<int>&v){
+    vector<int> ans;
+    int left_ptr=0;
+    int right_ptr=v.size()-1;
+
+    while(left_ptr<right_ptr){
+        if(abs(v[left_ptr])<abs(v[right_ptr])){
+            ans.push_back(v[right_ptr]*v[right_ptr]);
+            right_ptr--;
+        }
+        else{
+            ans.push_back(v[left_ptr]*v[left_ptr]);
+            left_ptr++;
+        }
+    }
+}
+
+int main ()
+{
+        int n;
+        cin>>n;
+        vector<int> v;
+        for(int i=0;i<n;i++){
+            int ele;
+            cin>>ele;
+            v.push_back(ele);
+        }
+            shortedSquaredArray(v);
+
+
+
+
+    return 0;
+
+}
